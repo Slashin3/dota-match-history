2a72e878b7efc879b6edfa7e41dadb288eae6e2a
Slashin3 <32717421+Slashin3@users.noreply.github.com>
2024-07-26 03:32:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const https = require("https");
+const { Client, IntentsBitField } = require("discord.js");
+
+const app = express();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`${c.user.username} is running`);
+});
+
+client.on("messageCreate", (message) => {
+  const url = "https://api.opendota.com/api/players/370370060/wl?limit=20";
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const playerData = JSON.parse(data);
+      const wins = Number(playerData.win);
+      const losses = Number(playerData.lose);
+      const total = wins + losses;
+      const mmrchange = wins * 26 - losses * 24;
+
+      if (message.content === "w") {
+        message.reply(`${wins}`);
+      } else if (message.content === "l") {
+        message.reply(`${losses}`);
+      } else if (message.content === "wl") {
+        message.reply(`${wins}-${losses} out of ${total} matches`);
+      } else if (message.content === "change") {
+        message.reply(`${mmrchange}`);
+      }
+    });
+  });
+});
+
+client.login(
+  "MTI2NjEzNTI5NzU0MDg4NjY4MA.G4F2u7.5Ygx0Yg9MkvkGJnz1apAFEr4d_HidoTKCtjvGw"
+);
